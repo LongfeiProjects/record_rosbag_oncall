48cf82faacb88e2f7a949b41ec1231a324081473
Longfei Zhao <longfeizhao@tencent.com>
2019-04-12 22:04:56 +0800

    auto trigger ros bag recorder works

--- scripts/my_rosbag_recorder.py
+++ scripts/my_rosbag_recorder.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import os
+import signal
 import datetime
 
 import rospy
@@ -20,7 +22,6 @@ class RosBagRecorder:
         self.bag_name_prefix = ''
         self.bagfile_dir = '/home/longfei/test_ws/src/record_rosbag_oncall/bagfiles'
         self.subprocess_pid = 0
-
         ##subscribe the imu and motordata
         rospy.Subscriber("/data_to_xavier", RobikeStatus, self.callback)
 
@@ -33,38 +34,35 @@ class RosBagRecorder:
     def callback(self, msg):
         status = msg.status
         status_bit_str = '{0:032b}'.format(status)
-        print('\n====================================')
-        print('status_bit_str is: ', status_bit_str)
+        rospy.logdebug('==================================== \n status_bit_str is: %s', status_bit_str)
 
         if status_bit_str[0] == '0':
             if self.is_recording:
-                rospy.loginfo('status_bit_str[0] = 0 and is recording')
+                rospy.logdebug('status_bit_str[0] = 0 and is recording')
                 try:
-                    kill_cmd = 'kill -SIGINT ' + str(self.subprocess_pid)
-                    rospy.loginfo('killed process with PID: %d', self.subprocess_pid)
+                    subprocess_group_pid = os.getpgid(self.subprocess_pid)
+                    os.killpg(os.getpgid(self.subprocess_pid), signal.SIGINT)
+
+                    rospy.loginfo('killed subprocess_group_pid: %d', subprocess_group_pid)
                     self.is_recording = False
                 except:
                     rospy.logerr('kill subprocess failed, subprocess_pid is %d', self.subprocess_pid)
-            else:
-                rospy.loginfo('status_bit_str[0] = 0 and is NOT recording')
-                pass
             
         elif status_bit_str[0] == '1':
             if not self.is_recording:
-                rospy.loginfo('status_bit_str[0] = 1 and is NOT recording')
+                rospy.logdebug('status_bit_str[0] = 1 and is NOT recording')
 
                 record_cmd = 'rosbag record /data_to_xavier -O ' + self.bag_name_prefix + datetime.datetime.now().strftime('%H%M%S-%Y%m%d') + '.bag'
                 rospy.loginfo('record_cmd is" $ ' + record_cmd)
 
-                p = subprocess.Popen(record_cmd, stdin=subprocess.PIPE, shell=True, cwd=self.bagfile_dir)
+                p = subprocess.Popen(record_cmd, stdin=subprocess.PIPE, shell=True, preexec_fn=os.setsid, cwd=self.bagfile_dir)
                 self.subprocess_pid = p.pid
                 rospy.loginfo('recorder started with PID: %d', self.subprocess_pid)
 
                 self.is_recording = True
                 self.bag_no += 1
-            else:
-                rospy.loginfo('status_bit_str[0] = 1 and is recording')
-                pass
+
+                rospy.loginfo('start to record ...')
         
         else:
             rospy.logerr('record_demand_str should be 32bit integer')
